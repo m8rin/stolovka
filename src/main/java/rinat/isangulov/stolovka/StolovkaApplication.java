8232f50fbf421546c8fb48356add4407735ed534
Исангулов Ринат <risangulov@blogic.ru>
2022-03-06 12:44:02 +0500

    first commit

---
+++ src/main/java/rinat/isangulov/stolovka/StolovkaApplication.java
@@ -0,0 +1,13 @@
+package rinat.isangulov.stolovka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StolovkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StolovkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rinat/isangulov/stolovka/StolovkaApplicationTests.java
@@ -0,0 +1,13 @@
+package rinat.isangulov.stolovka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StolovkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
